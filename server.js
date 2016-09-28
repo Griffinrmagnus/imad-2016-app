b31f70ae3b64044fc0343733b523759b329a346f
Griffinrmagnus <rahul.saji@gmail.com>
2016-09-28 13:39:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/Article1',function(req,res){
-    res.send('Article 1 requested and will be deployed');
+    res.sendFile(path.join(__dirname, 'ui', 'Article1.html'));
 });
 app.get('/Article2',function(req,res){
     res.send('Article 2 requested and will be deployed');
